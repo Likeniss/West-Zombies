71ff3a2603432d5a7cd5c3167ebf3cf66c2eb93d
Vinicius Oliveira de Freitas <vinicius.vof@outlook.com>
2020-04-29 23:33:41 -0300

    Personalização do Menu

--- main.c
+++ main.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <windows.h>
 #include <locale.h>
 
+// Protótipo de Funções
+FILE * AbreArquivo(char modo, char caminho[30]);
+void FecharArquivo(FILE * arquivo);
+void Cadastra(char nome[30], int pontos);
+void Listar(int tamanho);
+void LinhaSuperior(int tamanho);
+void LinhaRodape(int tamanho);
+void MenuItem(int tamanho, char string[]);
+void LinhaHorizontal(int tamanho);
+void MontaMenu();
 void instrucoes();
 void infodev();
 void menuCadastro();
 void menu(int select);
+
+// Funções
+int main()
+{
+    int selecao;
+    system("cls");
+    system("color 6");
+    printf("\n");
+    MontaMenu();
+    printf("\nSelecione a opcao: ");
+    scanf("%d", &selecao);
+    menu(selecao);
+
+    return 0;
+}
 
 FILE * AbreArquivo(char modo, char caminho[30]){
     FILE *arquivo;
@@ -38,60 +63,88 @@ void Cadastra(char nome[30], int pontos){
     FecharArquivo(arquivo);
 }
 
-void Listar(){
+void Listar(int tamanho){
     FILE *arquivo;
     char nome[30];
     int pontos;
+    int i;
     arquivo = AbreArquivo('l', "nomes.txt");
 
-    while(!feof(arquivo)){
+    for(i = 0; i < tamanho; i++){
         fscanf(arquivo, "%s %d\n", &nome,&pontos);
-        printf("Nome: %s \n Pontos: %d \n ", nome, pontos);
+        printf("\nNome: %s \nPontos: %d \n", nome, pontos);
     }
     FecharArquivo(arquivo);
 
 }
-int main()
-{
-    int selecao;
-    setlocale(LC_ALL, "Portuguese");
-    system("cls");
-    printf("\n");
+void LinhaSuperior(int tamanho){
+    int i;
+    printf("%c", 201);
+    for(i=0; i < tamanho;i++){
+        printf("%c", 205);
+    }
+    printf("%c\n", 187);
+}
+void LinhaRodape(int tamanho){
+    int i;
+    printf("%c", 200);
+    for(i=0; i < tamanho;i++){
+        printf("%c", 205);
+    }
+    printf("%c", 188);
+}
+void MenuItem(int tamanho, char string[]){
 
-    printf("\nMenu Principal \n\n");
-    printf("1 - Iniciar Jogo \n");
-    printf("2 - Instruções do Jogo \n");
-    printf("3 - Ranking \n");
-    printf("4 - Informações do Desenvolvedor \n");
-    printf("Selecione a opção: ");
-    scanf("%d", &selecao);
+    printf("%c", 186);
 
-    menu(selecao);
-    return 0;
+        printf("%-*s", tamanho, string);
+
+
+    printf("%c\n", 186);
+}
+
+void LinhaHorizontal(int tamanho){
+    int i;
+    printf("%c", 204);
+    for(i = 0; i < tamanho; i++)
+        printf("%c", 205);
+    printf("%c\n", 185);
+}
+
+void MontaMenu(){
+    LinhaSuperior(40);
+    MenuItem(40, "Menu Principal");
+    LinhaHorizontal(40);
+    MenuItem(40, "1 - Iniciar Jogo");
+    MenuItem(40, "2 - Instrucoes do Jogo");
+    MenuItem(40, "3 - Ranking");
+    MenuItem(40, "4 - Informacoes do Desenvolvedor");
+    MenuItem(40, "5 - Sair");
+    LinhaRodape(40);
 }
 
 void instrucoes(){
     system("cls");
-    printf("\n Instruções do Jogo \n");
-    printf("\n O objetivo do jogo é matar os zombies e \n sobreviver aos barris que vem em sua direção.");
+    printf("\nInstrucoes do Jogo \n");
+    printf("\nO objetivo do jogo e matar os zombies e \n sobreviver aos barris que vem em sua direcao.");
     printf("\n");
-    printf("\n - Para atirar, aperte a tecla espaço.");
-    printf("\n - Para pular, aperte a tecla direcional para cima. \n");
-    printf("\n \n Aperte qualquer tecla para voltar...");
+    printf("\n- Para atirar, aperte a tecla espaco.");
+    printf("\n- Para pular, aperte a tecla direcional para cima. \n");
+    printf("\n\nAperte qualquer tecla para voltar...");
     system("pause>null");
     main();
 }
 void infodev(){
     system("cls");
-    printf("\n Informações do Desenvolvedor");
+    printf("\nInformacoes do Desenvolvedor");
     printf("\n ");
-    printf("\n Desenvolvido por alunos da Faculdade Uniamérica: \n");
-    printf("\n - Vinicius Oliveira");
-    printf("\n - Larissa Lazzari");
-    printf("\n - Oliver J. Godoy");
-    printf("\n - Gabriel Hoffman");
+    printf("\nDesenvolvido por alunos da Faculdade Uniamerica: \n");
+    printf("\n- Vinicius Oliveira");
+    printf("\n- Larissa Lazzari");
+    printf("\n- Oliver J. Godoy");
+    printf("\n- Gabriel Hoffman");
     printf("\n");
-    printf("\n \n Aperte qualquer tecla para voltar...");
+    printf("\n\nAperte qualquer tecla para voltar...");
     system("pause>null");
     main();
 }
@@ -102,11 +155,19 @@ void menuCadastro(){
     printf("\nDigite seu nome: ");
     setbuf(stdin, NULL);
     gets(nome);
-    printf("\nDigite uma quantidade de pontos:");
+    printf("\nDigite uma quantidade de pontos: ");
     scanf("%d", &pontos);
     Cadastra(nome,pontos);
 
     play(nome);
+}
+void ranking(){
+    system("cls");
+    printf("Ranking \n");
+    Listar(5);
+    printf("\nAperte qualquer tecla para voltar...");
+    system("pause>null");
+    main();
 }
 void play(char nome[]){
     system("cls");
@@ -129,12 +190,13 @@ void menu(int select){
         break;
     ;
     case 3:
-
-        printf("Ranking \n");
-        Listar();
+        ranking();
         break;
     case 4:
         infodev();
+        break;
+    case 5:
+        exit(0);
         break;
     default:
         main();
